ac98cc07f12fad8dc64103d3cba1d72110f9be69
alex Heaven <94668119+wolovemadang@users.noreply.github.com>
2022-01-16 18:05:27 +0800

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -4,5 +4,5 @@ echo "Download ngrok"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP 
+#read -p "Paste Ngrok Authtoken: " CRP
+./ngrok 23KE1QqMII3UDnPOa4uleYgvRcl_5L7bNgSRteRyL4PCUonmF  /dev/null 2>&1 
